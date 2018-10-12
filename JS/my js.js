3b480d37d27e876c582f9e324296b197f55dd289
Julio Cesar <jrabanalg13@unc.edu.pe>
2018-10-11 23:37:51 -0500

    comit2

--- JS/my js.js
+++ JS/my js.js
@@ -1,6 +1,18 @@
-function changeImage() {
+(function() {
 
-    var pokeballImg = document.getElementById("icon01");
-    pokeballImg.src = "images/mystery.gif";
+    var flag = 0;
+    document.getElementById("btn").onclick = function() {
 
-}
+        var pokeballImg = document.getElementById("icon01");
+
+        if (flag == 0) {
+            pokeballImg.src = "/JulioRabanalG.github.io/imagenes/pokebola2.jpg";
+            flag = 1;
+        } else {
+            pokeballImg.src = " /JulioRabanalG.github.io/imagenes/pokebola1.jpg";
+            flag = 0;
+        }
+
+    }
+
+}());
